4ae7d2da997adbf5ebbfc165428ef2e350393bbf
William Cellegin <73302187+WillOzBR@users.noreply.github.com>
2023-05-30 20:26:22 -0300

    Adicionando botões e operações

--- Calculadora/php/Calc.php
+++ Calculadora/php/Calc.php
@@ -14,9 +14,50 @@ class Calc{
 
     public function somar(float $a, float $b){
         $this->setC($this->getA()+$this->getB());
-		print("O valor de c é = {$this->getC()}");
+		print("O valor da soma de {$this->getA()} + {$this->getB()} é = {$this->getC()}<br>");
 		return $this;
     }
+
+	public function subtrair(float $a, float $b){
+        $this->setC($this->getA()-$this->getB());
+		print("O valor da subtração de {$this->getA()} - {$this->getB()} é = {$this->getC()}<br>");
+		return $this;
+    }
+
+	public function multiplicar(float $a, float $b){
+        $this->setC($this->getA()*$this->getB());
+		print("O valor da multiplicação de {$this->getA()} x {$this->getB()} é = {$this->getC()}<br>");
+		return $this;
+    }
+
+	public function dividir(float $a, float $b){
+		if($this->getB()==0){
+			print("Erro! Divisão por 0!");
+		} else{
+        $this->setC($this->getA()/$this->getB());
+		print("O valor da divisão de {$this->getA()} / {$this->getB()} é = {$this->getC()}<br>");
+		return $this;
+		}
+		
+    }
+
+	public function exponenciar(float $a, float $b){
+        $this->setC(pow($this->getA(),$this->getB()));
+		print("O valor da exponenciação de {$this->getA()} elevado a {$this->getB()} é = {$this->getC()}<br>");
+		return $this;
+    }
+
+	public function acharRaiz(float $a, float $b){
+		if($this->getA()!=0){
+			$this->setC(sqrt($this->getA()));
+			print("O valor da raiz de {$this->getA()} é = {$this->getC()}<br>");
+		}else if($this->getB()!=0){
+			$this->setC(sqrt($this->getB()));
+			print("O valor da raiz de {$this->getB()} é = {$this->getC()}<br>");
+		}
+		return $this;
+    }
+
 
     /**
 	 * @return mixed
--- Calculadora/php/calculadora.php
+++ Calculadora/php/calculadora.php
@@ -15,11 +15,32 @@
     <div class="resultado">
         <?php
          require_once "./Calc.php";
+         error_reporting(E_ERROR | E_PARSE);
          $calculadora = new Calc($_GET['num1'],$_GET['num2']);
-         
+         $soma = $_GET['somar'];
+         $sub = $_GET['subtrair'];
+         $mult = $_GET['multiplicar'];
+         $div = $_GET['dividir'];
+         $exp = $_GET['elevar'];
+         $raiz = $_GET['acharRaiz'];
+
          print("O Valor de a é {$calculadora->getA()}<br>");
          print("O Valor de b é {$calculadora->getB()}<br>");
+         if (isset($soma)){
          $calculadora->somar($calculadora->getA(), $calculadora->getB());
+         } else if(isset($sub)){
+         $calculadora->subtrair($calculadora->getA(), $calculadora->getB());
+         } else if(isset($mult)){
+         $calculadora->multiplicar($calculadora->getA(), $calculadora->getB());
+         }else if(isset($div)){
+         $calculadora->dividir($calculadora->getA(), $calculadora->getB());
+         }else if(isset($exp)){
+         $calculadora->exponenciar($calculadora->getA(), $calculadora->getB());
+         }else if(isset($raiz)){
+         $calculadora->acharRaiz($calculadora->getA(), $calculadora->getB());
+        }
+
+       
         ?>
     </div>
 </body>
